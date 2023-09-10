7899fda373e5388818b04793330300f6c155e24a
DESKTOP-ENENB64\Bibo W\github <ideabosque@gmail.com>
2023-09-10 14:34:04 -0700

    Updated README.md and added pyproject.toml.

---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+# NetSuite-GraphQL-Engine
+=====================
+
+"""
+from setuptools import find_packages, setup
+
+setup(
+    name="NetSuite-GraphQL-Engine",
+    version="0.0.1.beta",
+    url="https://github.com/ideabosque/netsuite_graphql_engine",
+    license="MIT",
+    author="Idea Bosque",
+    author_email="ideabosque@gmail.com",
+    description="The **NetSuite GraphQL Engine** enables Python developers to interact with the NetSuite SuiteTalk API using GraphQL, simplifying data access and integration into their applications.",
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "SuiteTalk-Connector",
+        "graphene",
+        "pynamodb",
+        "boto3",
+        "silvaengine_utility",
+        "deepdiff",
+        "inspect",
+    ],
+    keywords=[
+        "GraphQL",
+        "NetSuite",
+        "SuiteTalk",
+        "Integration",
+        "API",
+    ],  # arbitrary keywords
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
